536eb9ee948d86563f2dc49fca863cd24c60df87
David Nicholas <dave@davidmn.org>
2014-02-25 09:45:25 +0000

    sun now at 0,0,0 and readme updated

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ nbody
 =====
 
 n-body simulations of the solar system
+
+basic2bodies
+------------
+
--- basic2bodies.f90
+++ basic2bodies.f90
@@ -3,29 +3,30 @@ program nbody
 
 	integer :: i,j,n
 	double precision :: G, dt, rad
-	double precision, DIMENSION(3, 2) :: r,v,a
+	! vectors in format (x1,y1,z1), (x2,y2,z2)
+	double precision, DIMENSION(3, 2) :: pos,v,a
 	double precision, DIMENSION(2) :: m
-	double precision, DIMENSION(3) :: dvector
-	dt = 1 ! time interval
+	double precision, DIMENSION(3) :: r
+	dt = 1 ! time interval (s)
 	n = 2
-	G = 6.67e-11 ! G Nm**2/kg**2
-	r = 0.0 ! coordinates
-	v = 0.0 ! velocities
-	a = 0.0 ! acceleration
-	dvector = 0.0 ! direction of the gravitational force
-	m(1) = 1.99e30 ! mass sun kg
-	m(2) = 5.97e24 ! mass earth kg
-	r(1,2) = 1.496e11 ! earth sun distance m
-	v(1,2) = 29.8e3 ! earth velocity km/s
+	G = 6.67e-11 ! G (Nm**2/kg**2)
+	pos = 0.0 ! position coordinates (m)
+	v = 0.0 ! velocities (m/s)
+	a = 0.0 ! acceleration (m/s*2)
+	r = 0.0 ! direction of the gravitational force
+	m(1) = 1.99e30 ! mass sun (kg)
+	m(2) = 5.97e24 ! mass earth (kg)
+	pos(2,1) = 1.496e11 ! earth sun distance (m)
+	v(2,2) = 29.8e3 ! earth velocity (m/s)
 	
 	do i = 1,n
 		do j = 1,n
 			if (j == i) CYCLE
-			dvector(1:3) = r(1:3,i)-r(1:3,j)
-			rad = sqrt((r(1,i)-r(1,j))**2 + (r(2,i)-r(2,j))**2 + (r(3,i)-r(3,j))**2)
-			a(1:3,j) = ((G * m(i))/rad**3)*dvector(1:3)
+			r(1:3) = pos(1:3,i)-pos(1:3,j)
+			rad = sqrt((pos(1,i)-pos(1,j))**2 + (pos(2,i)-pos(2,j))**2 + (pos(3,i)-pos(3,j))**2)
+			a(1:3,j) = ((G * m(i))/rad**3)*r(1:3)
 			v(1:3,j) = v(1:3,j) + a(1:3,j)*dt
-			r(1:3,j) = r(1:3,j) + v(1:3,j)*dt
+			pos(1:3,j) = pos(1:3,j) + v(1:3,j)*dt
 		end do
 	end do 
 	print *, rad
