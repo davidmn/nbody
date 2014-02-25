0346b6cc6689cca55f384c138e4d186dd1f5dd5f
David Nicholas <dave@davidmn.org>
2014-02-25 15:32:53 +0000

    changed initial conditions, still doesn't work

--- basic2bodies.f90
+++ basic2bodies.f90
@@ -1,12 +1,15 @@
 program nbody
 	implicit none
 
-	integer :: i,j,n
-	double precision :: G, dt, rad
+	integer :: i,j,n, counter
+	double precision :: G, dt, rad, year, t
 	! vectors in format (x1,y1,z1), (x2,y2,z2)
 	double precision, DIMENSION(3, 2) :: pos,v,a
 	double precision, DIMENSION(2) :: m
 	double precision, DIMENSION(3) :: r
+	counter = 0
+	t = 0.0
+	year = 3.15569e7 !number of seconds in a year
 	dt = 1 ! time interval (s)
 	n = 2
 	G = 6.67e-11 ! G (Nm**2/kg**2)
@@ -16,18 +19,24 @@ program nbody
 	r = 0.0 ! direction of the gravitational force
 	m(1) = 1.99e30 ! mass sun (kg)
 	m(2) = 5.97e24 ! mass earth (kg)
-	pos(2,1) = 1.496e11 ! earth sun distance (m)
+	pos(1,2) = 1.496e11 ! earth sun distance (m)
 	v(2,2) = 29.8e3 ! earth velocity (m/s)
-	
-	do i = 1,n
-		do j = 1,n
-			if (j == i) CYCLE
-			r(1:3) = pos(1:3,i)-pos(1:3,j)
-			rad = sqrt((pos(1,i)-pos(1,j))**2 + (pos(2,i)-pos(2,j))**2 + (pos(3,i)-pos(3,j))**2)
-			a(1:3,j) = ((G * m(i))/rad**3)*r(1:3)
-			v(1:3,j) = v(1:3,j) + a(1:3,j)*dt
-			pos(1:3,j) = pos(1:3,j) + v(1:3,j)*dt
+	open(unit=10, file="output")
+
+	do while (t < year)
+		do i = 1,n
+			do j = 1,n
+				if (j == i) CYCLE
+				r(1:3) = abs(pos(1:3,i)-pos(1:3,j))
+				rad = sqrt((pos(1,i)-pos(1,j))**2 + (pos(2,i)-pos(2,j))**2 + (pos(3,i)-pos(3,j))**2)
+				a(1:3,j) = ((G * m(i))/rad**3)*r(1:3)
+				v(1:3,j) = v(1:3,j) - a(1:3,j)*dt
+			end do
+			pos(1:3,i) = pos(1:3,i) + v(1:3,i)*dt
+			if (MOD(counter,1000000) == 0) write(10,*) pos(1:2,2)
+			counter = counter + 1
 		end do
+		t = t + dt
 	end do 
-	print *, rad
+	close(10)
 end program nbody
