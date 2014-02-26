20c821ae00daa5d2a9a2d53ccad5c3d3624832d5
davidmn <dave@davidmn.org>
2014-02-26 08:26:06 +0000

    fixed gravity, earth has circular orbit but sun seems off

--- basic2bodies.f90
+++ basic2bodies.f90
@@ -9,7 +9,7 @@ program nbody
 	double precision, DIMENSION(3) :: r
 	counter = 0
 	t = 0.0
-	year = 3.15569e7 !number of seconds in a year
+	year = 3.15570e7 !number of seconds in a year
 	dt = 1 ! time interval (s)
 	n = 2
 	G = 6.67e-11 ! G (Nm**2/kg**2)
@@ -21,22 +21,20 @@ program nbody
 	m(2) = 5.97e24 ! mass earth (kg)
 	pos(1,2) = 1.496e11 ! earth sun distance (m)
 	v(2,2) = 29.8e3 ! earth velocity (m/s)
-	open(unit=10, file="output")
 
 	do while (t < year)
 		do i = 1,n
 			do j = 1,n
 				if (j == i) CYCLE
-				r(1:3) = abs(pos(1:3,i)-pos(1:3,j))
+				r(1:3) = (pos(1:3,i)-pos(1:3,j))
 				rad = sqrt((pos(1,i)-pos(1,j))**2 + (pos(2,i)-pos(2,j))**2 + (pos(3,i)-pos(3,j))**2)
-				a(1:3,j) = ((G * m(i))/rad**3)*r(1:3)
+				a(1:3,j) = -((G * m(i))/rad**3)*r(1:3)
 				v(1:3,j) = v(1:3,j) - a(1:3,j)*dt
 			end do
 			pos(1:3,i) = pos(1:3,i) + v(1:3,i)*dt
-			if (MOD(counter,1000000) == 0) write(10,*) pos(1:2,2)
+			if (MOD(counter,1000000) == 0) print *, pos(1:2,1)
 			counter = counter + 1
 		end do
 		t = t + dt
 	end do 
-	close(10)
 end program nbody
