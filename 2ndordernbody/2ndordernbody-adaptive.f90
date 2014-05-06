51b9078347b723f9a361ef44a2e347adb5c30b0d
David Nicholas <dave@davidmn.org>
2014-05-06 09:52:09 +0100

    fixed some stuff, added integer time criteria

--- 2ndordernbody/2ndordernbody-adaptive.f90
+++ 2ndordernbody/2ndordernbody-adaptive.f90
@@ -8,9 +8,10 @@ program nbody
 	double precision, DIMENSION(3, 7) :: pos,v,a,a2
 	double precision, DIMENSION(7) :: m
 	double precision, DIMENSION(3) :: r,com,cov
-	integer, DIMENSION(6) :: class
+	integer, DIMENSION(7) :: class
+	uranus_distance = 0.0
 	AU = 1.5e11 ! astronomical unit
-	criteria = 0
+	criteria = 0.0
 	class = 1 ! start all planets having acc calculated every step, higher class are calculaed less often
 	counter = 0 ! used for printing
 	t = 0.0 ! the time
@@ -20,6 +21,7 @@ program nbody
 	G = 6.67e-11 ! G (Nm**2/kg**2)
 	pos = 0.0 ! position coordinates (m)
 	v = 0.0 ! velocities (m/s)
+	a2 = 0.0 
 	a = 0.0 ! acceleration (m/s*2)
 	r = 0.0 ! direction of the gravitational force
 	m(1) = 1.99e30 ! mass sun (kg)
@@ -34,6 +36,7 @@ program nbody
 	pos(1,4) = 2.387e11 ! mars sun distance (m)
 	pos(1,5) = 7.835e11 ! juptier sun distance (m)
 	pos(1,6) = 1.485e12 ! saturn sun distance (m)
+	pos(1,7) = 3.124e12 ! uranus sun distance (m)
 	v(2,2) = 3.5e4 ! venus velocity (m/s)
 	v(2,3) = 2.98e4 ! earth velocity (m/s)
 	v(2,4) = 2.41e4 ! mars velocity (m/s)
@@ -43,13 +46,12 @@ program nbody
 	com = 0.0 ! centre of mass array
 	cov = 0.0 ! centre of velocity array
 	m_tot = 0.0 ! total mass of system
-	e_0 = 0.0 ! total energy at time zero
+	e_0 = 0.0 ! total energy at time
 	e_t = 0.0 ! total energy at later time
 	u_temp = 0.0 ! temp potential energy
 	u_tot = 0.0 ! total potential energy
 	ke_tot = 0.0 ! total kinetic energy
 	ke_temp = 0.0 ! temp kinetic energy
-	a2 = a
 
 	! calculate adjustments for changing frames
 	do i = 1, n, 1
@@ -90,6 +92,7 @@ program nbody
 	ke_tot = 0.0
 	u_tot = 0.0
 
+	! kick
 	do i = 1,n
 		do j = 1,n
 			if (j == i) CYCLE
@@ -97,6 +100,22 @@ program nbody
 			rad = sqrt((pos(1,i)-pos(1,j))**2 + (pos(2,i)-pos(2,j))**2 + (pos(3,i)-pos(3,j))**2) ! length of r
 			a(1:3,i) = a(1:3,i) - ((G * m(j))/rad**3)*r(1:3) ! acceleration ma = Gmm/r^3 * rvector
 		end do
+	end do
+
+	! works out what integer time each body should be in
+	do i = 1, n, 1
+		criteria = 0.01 / sqrt(a(1,i)*a(1,i) + a(2,i)*a(2,i) + a(3,i))
+		print *, criteria
+		if ( criteria < 2.0 ) class(i) = 1
+		if ( criteria > 2.0 .and. criteria < 4.0 ) class(i) = 2
+		if ( criteria > 4.0 .and. criteria < 8.0 ) class(i) = 4
+		if ( criteria > 8.0 .and. criteria < 16.0 ) class(i) = 8
+		if ( criteria > 16.0 .and. criteria < 32.0 ) class(i) = 16
+		if ( criteria > 32.0 ) class(i) = 32		
+	end do
+
+	do i = 1, n, 1
+		print *, class(i)
 	end do
 
 	! main loop, loops over time
@@ -141,8 +160,8 @@ program nbody
 				e_t = u_tot + ke_tot
 				! energy difference
 				current_time = t/year
-				!print *, ((e_t-e_0)/e_0)*100, "time  =", current_time
-				uranus_distance = pos(1,1)
+				uranus_distance = (sqrt((pos(1,1)-pos(1,7))**2 + (pos(2,1)-pos(2,7))**2 + (pos(3,1)-pos(3,7))**2))/AU
+				print *, "time =", current_time, ((e_t-e_0)/e_0)*100
 				print *, "time =", current_time, uranus_distance
 				counter = 0
 			end if
